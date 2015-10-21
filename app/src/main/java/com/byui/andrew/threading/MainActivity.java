2e256de4bc715933b9fe5682c2e1ea0758e98156
thurmand <twintrool@gmailcom>
2015-10-21 16:35:21 -0600

    Initial commit

---
+++ app/src/main/java/com/byui/andrew/threading/MainActivity.java
@@ -0,0 +1,22 @@
+package com.byui.andrew.threading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void create(View v){
+        
+        File file = new File(context.getFilesDir(), filename);
+    }
+
+}
